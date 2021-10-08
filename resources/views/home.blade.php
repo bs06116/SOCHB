fa65143fd3084798814e77752c905d7d066d0be5
asim <iasimriaz@gmail.com>
2021-10-08 19:45:09 +0500

    dashboard changes

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,13 +2,13 @@
 @section('content')
     <div class="row">
         <div class="col-xl-3 col-md-6">
-            {{-- <div class="card card-stats">
+            <div class="card card-stats">
                 <!-- Card body -->
                 <div class="card-body">
                     <div class="row">
                         <div class="col">
-                            <h5 class="card-title text-uppercase text-muted mb-0">Total traffic</h5>
-                            <span class="h2 font-weight-bold mb-0">350,897</span>
+                            <h5 class="card-title text-uppercase text-muted mb-0">Total Admin</h5>
+                            <span class="h2 font-weight-bold mb-0">-</span>
                         </div>
                         <div class="col-auto">
                             <div class="icon icon-shape bg-gradient-red text-white rounded-circle shadow">
@@ -16,12 +16,9 @@
                             </div>
                         </div>
                     </div>
-                    <p class="mt-3 mb-0 text-sm">
-                        <span class="text-success mr-2"><i class="fa fa-arrow-up"></i> 3.48%</span>
-                        <span class="text-nowrap">Since last month</span>
-                    </p>
+
                 </div>
-            </div> --}}
+            </div>
         </div>
         <div class="col-xl-3 col-md-6">
             <div class="card card-stats">
@@ -29,8 +26,8 @@
                 <div class="card-body">
                     <div class="row">
                         <div class="col">
-                            <h5 class="card-title text-uppercase text-muted mb-0">New users</h5>
-                            <span class="h2 font-weight-bold mb-0">2,356</span>
+                            <h5 class="card-title text-uppercase text-muted mb-0">Total Customer Serivce</h5>
+                            <span class="h2 font-weight-bold mb-0">-</span>
                         </div>
                         <div class="col-auto">
                             <div class="icon icon-shape bg-gradient-orange text-white rounded-circle shadow">
@@ -38,21 +35,18 @@
                             </div>
                         </div>
                     </div>
-                    <p class="mt-3 mb-0 text-sm">
-                        <span class="text-success mr-2"><i class="fa fa-arrow-up"></i> 3.48%</span>
-                        <span class="text-nowrap">Since last month</span>
-                    </p>
+
                 </div>
             </div>
         </div>
         <div class="col-xl-3 col-md-6">
-            {{-- <div class="card card-stats">
+            <div class="card card-stats">
                 <!-- Card body -->
                 <div class="card-body">
                     <div class="row">
                         <div class="col">
-                            <h5 class="card-title text-uppercase text-muted mb-0">Sales</h5>
-                            <span class="h2 font-weight-bold mb-0">924</span>
+                            <h5 class="card-title text-uppercase text-muted mb-0">Total Sales Manager</h5>
+                            <span class="h2 font-weight-bold mb-0">-</span>
                         </div>
                         <div class="col-auto">
                             <div class="icon icon-shape bg-gradient-green text-white rounded-circle shadow">
@@ -60,21 +54,18 @@
                             </div>
                         </div>
                     </div>
-                    <p class="mt-3 mb-0 text-sm">
-                        <span class="text-success mr-2"><i class="fa fa-arrow-up"></i> 3.48%</span>
-                        <span class="text-nowrap">Since last month</span>
-                    </p>
+
                 </div>
-            </div> --}}
+            </div>
         </div>
         <div class="col-xl-3 col-md-6">
-            {{-- <div class="card card-stats">
+            <div class="card card-stats">
                 <!-- Card body -->
                 <div class="card-body">
                     <div class="row">
                         <div class="col">
-                            <h5 class="card-title text-uppercase text-muted mb-0">Performance</h5>
-                            <span class="h2 font-weight-bold mb-0">49,65%</span>
+                            <h5 class="card-title text-uppercase text-muted mb-0">Total WH Manager</h5>
+                            <span class="h2 font-weight-bold mb-0">-</span>
                         </div>
                         <div class="col-auto">
                             <div class="icon icon-shape bg-gradient-info text-white rounded-circle shadow">
@@ -82,12 +73,28 @@
                             </div>
                         </div>
                     </div>
-                    <p class="mt-3 mb-0 text-sm">
-                        <span class="text-success mr-2"><i class="fa fa-arrow-up"></i> 3.48%</span>
-                        <span class="text-nowrap">Since last month</span>
-                    </p>
+
                 </div>
-            </div> --}}
+            </div>
+        </div>
+        <div class="col-xl-3 col-md-6">
+            <div class="card card-stats">
+                <!-- Card body -->
+                <div class="card-body">
+                    <div class="row">
+                        <div class="col">
+                            <h5 class="card-title text-uppercase text-muted mb-0">Total Team Superviosr</h5>
+                            <span class="h2 font-weight-bold mb-0">-</span>
+                        </div>
+                        <div class="col-auto">
+                            <div class="icon icon-shape bg-gradient-info text-white rounded-circle shadow">
+                                <i class="ni ni-chart-bar-32"></i>
+                            </div>
+                        </div>
+                    </div>
+
+                </div>
+            </div>
         </div>
     </div>
 
